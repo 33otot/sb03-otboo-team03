4357e092387de4e99bc00ac3b0a9f7c6b0294cd0
BackEunHo <bhh0214@gmail.com>
2025-09-12 22:03:27 +0900

    chore: UserRepository에서 불필요한 import 제거

--- src/main/java/com/samsamotot/otboo/user/repository/UserRepository.java
+++ src/main/java/com/samsamotot/otboo/user/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.samsamotot.otboo.user.repository;
 
 import com.samsamotot.otboo.user.entity.Role;
 import com.samsamotot.otboo.user.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
