97598759b3fad1f1af00119054db6813c81c1ef8
HuInDoL <80470052+HuInDoL@users.noreply.github.com>
2025-09-12 22:39:38 +0900

    feat: Repository 올바르게 적용
    - LocationRepository.java
    - ProfileRepository.java
    - WeatherRepository.java

--- src/main/java/com/samsamotot/otboo/profile/repository/ProfileRepository.java
+++ src/main/java/com/samsamotot/otboo/profile/repository/ProfileRepository.java
@@ -2,10 +2,8 @@ package com.samsamotot.otboo.profile.repository;
 
 import com.samsamotot.otboo.profile.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
-public interface ProfileRepository extends JpaRepository<UUID, Profile> {
+public interface ProfileRepository extends JpaRepository<Profile, UUID> {
 }
