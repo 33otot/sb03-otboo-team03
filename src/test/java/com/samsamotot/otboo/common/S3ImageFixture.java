33aeb2b2eddc24458cdf9ac3db9c0559463b667e
wangcoJiin <mf0d070208@gmail.com>
2025-09-15 10:58:16 +0900

    add: S3 이미지 업로드 테스트를 위한 Fixture 클래스 추가

---
+++ src/test/java/com/samsamotot/otboo/common/S3ImageFixture.java
@@ -0,0 +1,45 @@
+package com.samsamotot.otboo.common;
+
+import org.springframework.mock.web.MockMultipartFile;
+
+/**
+ * S3 이미지 업로드 테스트 용 파일 생성하는 Fixture 클래스
+ */
+public class S3ImageFixture {
+
+    private static final String DEFAULT_FILE_NAME = "test.png";
+    private static final String DEFAULT_CONTENT_TYPE = "image/png";
+    private static final byte[] DEFAULT_CONTENT = "dummy-image-content".getBytes();
+
+    // 정상 파일
+    public static MockMultipartFile createSampleImageFile() {
+        return new MockMultipartFile(
+            "file",
+            DEFAULT_FILE_NAME,
+            DEFAULT_CONTENT_TYPE,
+            DEFAULT_CONTENT
+        );
+    }
+
+     // 비어있는 이미지 파일 Fixture
+    public static MockMultipartFile createEmptyImageFile() {
+        return new MockMultipartFile(
+            "file",
+            "empty.png",
+            DEFAULT_CONTENT_TYPE,
+            new byte[0]
+        );
+    }
+
+
+     // 잘못된 Content-Type의 파일 Fixture
+    public static MockMultipartFile createInvalidTypeFile() {
+        return new MockMultipartFile(
+            "file",
+            "test.txt",
+            "text/plain",
+            "dummy-text".getBytes()
+        );
+    }
+
+}
