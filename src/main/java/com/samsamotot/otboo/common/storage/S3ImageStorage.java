9c13f0a1a3442e64432d96ec4d1505061b5a013d
wangcoJiin <mf0d070208@gmail.com>
2025-09-16 10:23:28 +0900

    refactor: 로그 출력 형식 수정

--- src/main/java/com/samsamotot/otboo/common/storage/S3ImageStorage.java
+++ src/main/java/com/samsamotot/otboo/common/storage/S3ImageStorage.java
@@ -101,7 +101,7 @@ public class S3ImageStorage {
         try{
             return fileName.substring(fileName.lastIndexOf("."));
         } catch (StringIndexOutOfBoundsException e){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일" + fileName + ") 입니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일(" + fileName + ") 입니다.");
         }
     }
 
