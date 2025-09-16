c7ae00dbde6cd3c4507b2fd49ac02256a1fa2df9
wangcoJiin <mf0d070208@gmail.com>
2025-09-16 15:45:06 +0900

    add: ClothesAttributeDefApi, ClothesAttributeDefController 추가
    - 의상 속성 정의 생성 컨트롤러 메서드 추가

---
+++ src/main/java/com/samsamotot/otboo/clothes/controller/ClothesAttributeDefController.java
@@ -0,0 +1,33 @@
+package com.samsamotot.otboo.clothes.controller;
+
+import com.samsamotot.otboo.clothes.dto.ClothesAttributeDefDto;
+import com.samsamotot.otboo.clothes.dto.request.ClothesAttributeDefCreateRequest;
+import com.samsamotot.otboo.clothes.entity.ClothesAttributeDef;
+import com.samsamotot.otboo.clothes.service.ClothesAttributeDefService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/clothes/attribute-defs")
+@RestController
+public class ClothesAttributeDefController implements ClothesAttributeDefApi {
+
+    private final ClothesAttributeDefService defService;
+
+    @PostMapping
+    public ResponseEntity<ClothesAttributeDefDto> createClothesAttributeDef(
+        @Valid @RequestBody ClothesAttributeDefCreateRequest request
+    ) {
+        ClothesAttributeDefDto result = defService.create(request);
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(result);
+    }
+}
