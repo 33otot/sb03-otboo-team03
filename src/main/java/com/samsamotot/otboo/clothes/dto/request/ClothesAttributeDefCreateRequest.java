f577126873183e60e4b7be120b78a0067d8bf8c9
wangcoJiin <mf0d070208@gmail.com>
2025-09-16 11:14:50 +0900

    feat: ClothesAttributeDefCreateRequest에 값 검증 추가

--- src/main/java/com/samsamotot/otboo/clothes/dto/request/ClothesAttributeDefCreateRequest.java
+++ src/main/java/com/samsamotot/otboo/clothes/dto/request/ClothesAttributeDefCreateRequest.java
@@ -1,10 +1,17 @@
 package com.samsamotot.otboo.clothes.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record ClothesAttributeDefCreateRequest(
+    @NotBlank(message = "속성 정의의 이름은 필수입니다.")
     String name,
+
+    @NotNull(message = "옵션 리스트는 null일 수 없습니다.")
+    @Size(min = 1, message = "최소 1개 이상의 옵션이 필요합니다.")
     List<String> selectableValues
 ) { }
