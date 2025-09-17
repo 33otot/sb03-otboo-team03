bb8b1ce0e37966de93655d6004afe3a23511e0f8
wangcoJiin <mf0d070208@gmail.com>
2025-09-17 09:29:49 +0900

    fix: 리스트 원소까지 검사하는 validation으로 변경

--- src/main/java/com/samsamotot/otboo/clothes/dto/request/ClothesAttributeDefCreateRequest.java
+++ src/main/java/com/samsamotot/otboo/clothes/dto/request/ClothesAttributeDefCreateRequest.java
@@ -1,8 +1,7 @@
 package com.samsamotot.otboo.clothes.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.Builder;
 
@@ -11,7 +10,6 @@ public record ClothesAttributeDefCreateRequest(
     @NotBlank(message = "속성 정의의 이름은 필수입니다.")
     String name,
 
-    @NotNull(message = "옵션 리스트는 null일 수 없습니다.")
-    @Size(min = 1, message = "최소 1개 이상의 옵션이 필요합니다.")
-    List<String> selectableValues
+    @NotEmpty(message = "옵션 리스트는 비어 있을 수 없습니다.")
+    List<@NotBlank(message = "각 옵션 값은 공백일 수 없습니다.") String> selectableValues
 ) { }
