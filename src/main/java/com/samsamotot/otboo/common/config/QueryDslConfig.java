ccef790549d3201ee888c97c92777c69e0043869
김동욱 Daniel Kim <dongwook5478@gmail.com>
2025-09-18 15:48:01 +0900

    fix: 쿼리 dsl 파일이 두개로 인식하는 문제 해결

--- src/main/java/com/samsamotot/otboo/common/config/QueryDslConfig.java
+++ src/main/java/com/samsamotot/otboo/common/config/QueryDslConfig.java
@@ -1,5 +1,12 @@
 package com.samsamotot.otboo.common.config;
 
+/**
+ * PackageName  : com.samsamotot.otboo.common.config
+ * FileName     : QuerydslConfig
+ * Author       : dounguk
+ * Date         : 2025. 9. 18.
+ */
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
@@ -14,3 +21,4 @@ public class QueryDslConfig {
         return new JPAQueryFactory(em);
     }
 }
+
