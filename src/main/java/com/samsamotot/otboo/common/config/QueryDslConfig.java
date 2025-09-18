6769947dd94ad862430c4b819c08fd041ac04ffc
BackEunHo <bhh0214@gmail.com>
2025-09-18 18:01:32 +0900

    refactor: QuerydslConfig 클래스 이름 변경 및 관련 테스트 수정
    - QuerydslConfig 클래스를 QueryDslConfig로 이름 변경
    - 관련된 테스트 파일에서 클래스 이름을 업데이트하여 일관성 유지

--- src/main/java/com/samsamotot/otboo/common/config/QueryDslConfig.java
+++ src/main/java/com/samsamotot/otboo/common/config/QueryDslConfig.java
@@ -2,18 +2,19 @@ package com.samsamotot.otboo.common.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * PackageName  : com.samsamotot.otboo.common.config
+ * FileName     : QuerydslConfig
+ * Author       : dounguk
+ * Date         : 2025. 9. 15.
+ */
 @Configuration
-@RequiredArgsConstructor
 public class QueryDslConfig {
-
-    private final EntityManager entityManager;
-
     @Bean
-    public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(entityManager);
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 }
