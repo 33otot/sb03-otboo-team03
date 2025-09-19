488ee7b091aa7c004f654511e6f80d8543b43487
wangcoJiin <mf0d070208@gmail.com>
2025-09-19 09:28:47 +0900

    fix: 잘못된 HTTP 메서드 매핑 사용된 api 인터페이스 수정

--- src/main/java/com/samsamotot/otboo/clothes/controller/ClothesAttributeDefApi.java
+++ src/main/java/com/samsamotot/otboo/clothes/controller/ClothesAttributeDefApi.java
@@ -6,7 +6,6 @@ import com.samsamotot.otboo.clothes.dto.request.ClothesAttributeDefUpdateRequest
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -14,10 +13,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Tag(name = "의상 속성 정의", description = "의상 속성 정의 관련 API")
 public interface ClothesAttributeDefApi {
@@ -31,7 +29,7 @@ public interface ClothesAttributeDefApi {
                 content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
         }
     )
-    @GetMapping
+    @PostMapping
     ResponseEntity<ClothesAttributeDefDto> createClothesAttributeDef(
         @Parameter ClothesAttributeDefCreateRequest request
     );
