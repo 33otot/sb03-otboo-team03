17709cb0d7abc05b5d7c6fda2c496ec215b2d281
김동욱 Daniel Kim <dongwook5478@gmail.com>
2025-09-23 18:56:05 +0900

    feat: ws config 파일 추가

---
+++ src/main/java/com/samsamotot/otboo/common/config/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.samsamotot.otboo.common.config;
+
+/**
+ * PackageName  : com.samsamotot.otboo.common.config
+ * FileName     : WebSocketConfig
+ * Author       : dounguk
+ * Date         : 2025. 9. 23.
+ */
+public class WebSocketConfig {
+}
