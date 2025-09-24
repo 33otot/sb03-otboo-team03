c743ec8a5eb84c4667b961fdbf93ee947657f399
HuInDoL <80470052+HuInDoL@users.noreply.github.com>
2025-09-24 13:00:00 +0900

    feature: 비동기 및 스케줄러 설정
    - AsyncConfig.java
    - WeatherScheduler.java

--- src/main/java/com/samsamotot/otboo/OtbooApplication.java
+++ src/main/java/com/samsamotot/otboo/OtbooApplication.java
@@ -2,8 +2,10 @@ package com.samsamotot.otboo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OtbooApplication {
 
 	public static void main(String[] args) {
