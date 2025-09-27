9da302ab52687f5f99e2c06a763b9538996e3168
wangcoJiin <mf0d070208@gmail.com>
2025-09-27 18:59:27 +0900

    feat: 커스텀 레포지토리를 extends 하도록 ClothesRepository 인터페이스 수정

--- src/main/java/com/samsamotot/otboo/clothes/repository/ClothesRepository.java
+++ src/main/java/com/samsamotot/otboo/clothes/repository/ClothesRepository.java
@@ -1,11 +1,12 @@
 package com.samsamotot.otboo.clothes.repository;
 
 import com.samsamotot.otboo.clothes.entity.Clothes;
+import com.samsamotot.otboo.clothes.repository.custom.ClothesRepositoryCustom;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClothesRepository extends JpaRepository<Clothes, UUID> {
+public interface ClothesRepository extends JpaRepository<Clothes, UUID>, ClothesRepositoryCustom {
 
 }
