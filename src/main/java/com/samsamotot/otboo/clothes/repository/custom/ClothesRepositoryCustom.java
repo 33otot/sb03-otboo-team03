f86e33948c92208aec80f757cb2c916cda61e637
wangcoJiin <mf0d070208@gmail.com>
2025-09-29 21:45:26 +0900

    refactor: 의상 목록 조회시 인증객체에서 추출한 id로 비교하도록 BooleanBuilder의 조건 수정

--- src/main/java/com/samsamotot/otboo/clothes/repository/custom/ClothesRepositoryCustom.java
+++ src/main/java/com/samsamotot/otboo/clothes/repository/custom/ClothesRepositoryCustom.java
@@ -2,13 +2,14 @@ package com.samsamotot.otboo.clothes.repository.custom;
 
 import com.samsamotot.otboo.clothes.dto.request.ClothesSearchRequest;
 import com.samsamotot.otboo.clothes.entity.Clothes;
+import java.util.UUID;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface ClothesRepositoryCustom {
     // 목록 조회
-    Slice<Clothes> findClothesWithCursor(ClothesSearchRequest request, Pageable pageable);
+    Slice<Clothes> findClothesWithCursor(UUID ownerId, ClothesSearchRequest request, Pageable pageable);
 
     // totalElement 구하기
-    long totalElementCount(ClothesSearchRequest request);
+    long totalElementCount(UUID ownerId, ClothesSearchRequest request);
 }
