a94d13778b04a368612b1e4b07f71b9d7b0a125d
HuInDoL <80470052+HuInDoL@users.noreply.github.com>
2025-09-30 16:10:56 +0900

    bugfix: 잘못 병합된 부분 수정

--- src/main/java/com/samsamotot/otboo/profile/repository/ProfileRepository.java
+++ src/main/java/com/samsamotot/otboo/profile/repository/ProfileRepository.java
@@ -1,7 +1,6 @@
 package com.samsamotot.otboo.profile.repository;
 
 import com.samsamotot.otboo.profile.entity.Profile;
-import com.samsamotot.otboo.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
