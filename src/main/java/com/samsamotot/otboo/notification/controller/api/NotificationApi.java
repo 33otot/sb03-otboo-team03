ddf8807b089797e4d7e4ed3d339b676d9c5e485f
김동욱 Daniel Kim <dongwook5478@gmail.com>
2025-10-01 14:30:44 +0900

    docs: 알림 swagger 추가

---
+++ src/main/java/com/samsamotot/otboo/notification/controller/api/NotificationApi.java
@@ -0,0 +1,40 @@
+package com.samsamotot.otboo.notification.controller.api;
+
+import com.samsamotot.otboo.notification.dto.NotificationListResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * PackageName  : com.samsamotot.otboo.notification.controller.api
+ * FileName     : NotificationApi
+ * Author       : dounguk
+ * Date         : 2025. 10. 1.
+ */
+
+@Tag(name = "알림", description = "알림 API")
+public interface NotificationApi {
+
+    @Operation(summary = "알림 목록 조회 API")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "알림 목록 조회 성공",
+            content = @Content(schema = @Schema(implementation = NotificationListResponse.class))
+        ),
+        @ApiResponse(responseCode = "400", description = "알림 목록 조회 실패", content = @Content)
+    })
+    @GetMapping
+    ResponseEntity<NotificationListResponse> getNotifications(
+        @RequestParam(required = false) Instant cursor,
+        @RequestParam(required = false) UUID idAfter,
+        Integer limit
+    );
+}
