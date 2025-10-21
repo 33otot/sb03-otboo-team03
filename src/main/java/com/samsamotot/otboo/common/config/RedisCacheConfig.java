922e817f2bf472475511cf7fe580f61e7707f4c8
wangcoJiin <mf0d070208@gmail.com>
2025-10-21 10:26:00 +0900

    test: 테스트 빌드 성공을 위한 설정 수정

--- src/main/java/com/samsamotot/otboo/common/config/RedisCacheConfig.java
+++ src/main/java/com/samsamotot/otboo/common/config/RedisCacheConfig.java
@@ -1,17 +1,16 @@
 package com.samsamotot.otboo.common.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.samsamotot.otboo.common.util.CacheNames;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -19,13 +18,10 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-
 @Slf4j
 @Configuration
 @EnableCaching
+@Profile("!test")
 public class RedisCacheConfig {
 
     private static final String CONFIG_NAME = "[RedisCacheConfig] ";
--- src/main/java/com/samsamotot/otboo/common/config/RedisConfig.java
+++ src/main/java/com/samsamotot/otboo/common/config/RedisConfig.java
@@ -26,6 +26,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Profile("!test")
 @RequiredArgsConstructor
 @Slf4j
 public class RedisConfig {
