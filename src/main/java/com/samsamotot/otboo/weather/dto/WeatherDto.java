16abcb60ae996ef272cba8180e6975fc95c89a9e
wangcoJiin <mf0d070208@gmail.com>
2025-10-21 10:15:18 +0900

    fix: 엘라스틱 서치를 위해 WeatherDto에 @ Field 추가

--- src/main/java/com/samsamotot/otboo/weather/dto/WeatherDto.java
+++ src/main/java/com/samsamotot/otboo/weather/dto/WeatherDto.java
@@ -4,11 +4,17 @@ import com.samsamotot.otboo.weather.entity.SkyStatus;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Builder;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Builder
 public record WeatherDto(
     UUID id,
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
     LocalDateTime forecastedAt,
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
     LocalDateTime forecastAt,
     WeatherAPILocation location,
     SkyStatus skyStatus,
