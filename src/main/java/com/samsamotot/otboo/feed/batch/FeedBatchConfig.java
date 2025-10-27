924dff98e3baae465ab58833ee3c9032c1f5d97d
jhlee-codes <jhleezz00@naver.com>
2025-10-27 17:51:29 +0900

    refactor: 코드래빗 리뷰사항 반영

--- src/main/java/com/samsamotot/otboo/feed/batch/FeedBatchConfig.java
+++ src/main/java/com/samsamotot/otboo/feed/batch/FeedBatchConfig.java
@@ -18,6 +18,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
@@ -25,6 +26,7 @@ import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.support.PostgresPagingQueryProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -56,15 +58,20 @@ public class FeedBatchConfig {
                 new String[] {"deadline"},
                 new String[] {}
             ))
+            .incrementer(new RunIdIncrementer())
             .build();
     }
 
     // Step 정의
     @Bean
-    public Step deleteSoftDeletedFeedsStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step deleteSoftDeletedFeedsStep(
+        JobRepository jobRepository,
+        PlatformTransactionManager transactionManager,
+        @Qualifier(FeedBatchConfig.READER_NAME) JdbcPagingItemReader<UUID> softDeletedFeedReader
+    ) {
         return new StepBuilder(STEP_NAME, jobRepository)
             .<UUID, UUID>chunk(chunkSize, transactionManager)
-            .reader(softDeletedFeedReader(null))
+            .reader(softDeletedFeedReader)
             .writer(softDeletedFeedWriter())
             .build();
     }
