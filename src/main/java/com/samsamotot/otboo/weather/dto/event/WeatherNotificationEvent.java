df170ba52ad1bb7e93eb66bffa9b3c3634285c07
HuInDoL <80470052+HuInDoL@users.noreply.github.com>
2026-02-06 14:38:46 +0900

    feat: 날씨 알림 이벤트 레코드 생성

--- src/main/java/com/samsamotot/otboo/weather/dto/event/WeatherNotificationEvent.java
+++ src/main/java/com/samsamotot/otboo/weather/dto/event/WeatherNotificationEvent.java
@@ -2,15 +2,16 @@ package com.samsamotot.otboo.weather.dto.event;
 
 import com.samsamotot.otboo.notification.entity.NotificationLevel;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
- * PackageName  : com.samsamotot.otboo.notification.dto.event
+ * PackageName  : com.samsamotot.otboo.weather.dto.event
  * FileName     : WeatherNotificationEvent
  * Description  : 날씨 변화 감지 시 발송되는 알림 이벤트
  */
 public record WeatherNotificationEvent(
-        UUID receiverId,
+        List<UUID> receiverId,
         String title,
         String content,
         NotificationLevel level
