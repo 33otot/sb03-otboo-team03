8f71f0a3fe626f666e63a7506339250b68822d09
HuInDoL <80470052+HuInDoL@users.noreply.github.com>
2026-02-23 13:25:00 +0900

    log: CircuitWeatherClient 간단 로깅 및 이름 변경

--- src/main/java/com/samsamotot/otboo/weather/client/CircuitWeatherClient.java
+++ src/main/java/com/samsamotot/otboo/weather/client/CircuitWeatherClient.java
@@ -12,13 +12,15 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Primary // WeatherClient 주입 시 이 클래스가 우선순위를 가짐
 @Component
-public class ResilientWeatherClient implements WeatherClient {
+public class CircuitWeatherClient implements WeatherClient {
+
+    private static final String CLIENT_NAME = "[CircuitWeatherClient] ";
 
     private final WeatherClient kmaClient;
     private final WeatherClient openWeatherMapClient;
     private final ReactiveCircuitBreaker circuitBreaker;
 
-    public ResilientWeatherClient(
+    public CircuitWeatherClient(
             @Qualifier("kmaClient") WeatherClient kmaClient,
             @Qualifier("openWeatherMapClient") WeatherClient openWeatherMapClient,
             ReactiveCircuitBreakerFactory circuitBreakerFactory
@@ -34,7 +36,7 @@ public class ResilientWeatherClient implements WeatherClient {
         return circuitBreaker.run(
                 kmaClient.fetchWeather(nx, ny), // 1. 메인(기상청) 호출 시도
                 throwable -> { // 2. 실패 시 Fallback 실행
-                    log.warn("⚠️ KMA API Failed! Switching to OpenWeatherMap... Error: {}", throwable.getMessage());
+                    log.warn(CLIENT_NAME + "⚠️ KMA API Failed! Switching to OpenWeatherMap... Error: {}", throwable.getMessage());
                     // 에러 발생 시 서브(OWM) 호출
                     return openWeatherMapClient.fetchWeather(nx, ny);
                 }
